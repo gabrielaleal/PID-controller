626b1c8a121bca2b38366f300b20f411e163199d
gabrielaleal <gabriela.leal@hotmail.com>
2018-11-29 12:00:56 -0300

    Update

--- PIDcontroller.m
+++ PIDcontroller.m
@@ -7,14 +7,22 @@ Kp = 350;
 Kd = 300;
 Ki = 30;
 
+l = 20*ones(50,1).'; % aux vector
+n = 40*ones(50,1).'; % aux vector
+u = cat(2, l, n);
+%{
+  using the vector u we change the wanted speed
+  in the middle of the interval
+%}
+
 s = tf('s');
 P = 1/(m*s+b); % transfer function (plant)
 C = pid(Kp, Ki, Kd); % controller
 
 sys = series(C, P); % since the controller and the plant are in series
 
-T = feedback(sys,1, -1);
+T = feedback(sys,1, -1); % we obtained the feedback system
 
 % plot graph
-t = 0 : 0.5 : 100; % interval
-step(20*T, t) % as 20 means the speed we want to reach
+t = 0.5 : 0.5 : 50; % interval
+lsim(T, u, t);
